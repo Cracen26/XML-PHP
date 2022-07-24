8606da28b56832c58ced2a01ee50ba321c307672
falilou <mouhamadoufalilouball@esp.sn>
2022-07-24 19:58:25 +0100

    ref

--- client.php
+++ client.php
@@ -19,69 +19,85 @@
         </nav>
 </div>
 <br>
-<div class="container">
-    <div class="row text-center bg-light">
-        <h3>Cinema</h3>
-    </div>
-    <div class="row">
-        <div class="col-sm-6">
-            <img src="images/v.svg" class="img-fluid" alt="">
-        </div>
-        <div class="col-sm-6 bg-info" style="opacity:70%;">
-            <?php
-                if (file_exists('xml/cinema.xml')) {
-                    $xml = simplexml_load_file('xml/cinema.xml');
-                    echo $xml->film[0]->synopsis;
-                } else {
-                    exit('Echec lors de l\'ouverture du fichier test.xml.');
-                }
-            ?>
-        </div>
-    
-    </div>
-    <br>
-    <div class="row text-center bg-light">
-        <h3>Examen</h3>
-    </div>
-    <div class="row">
-        <div class="col-sm-6">
-            <img src="images/v.svg" class="img-fluid" alt="">
-        </div>
-        <div class="col-sm-6 bg-info" style="opacity:70%;">
-            <?php
-                if (file_exists('xml/cinema.xml')) {
-                    $xml = simplexml_load_file('xml/cinema.xml');
-                    echo $xml->film[0]->synopsis;
-                } else {
-                    exit('Echec lors de l\'ouverture du fichier test.xml.');
-                }
-            ?>
-        </div>
-    
-    </div>
-    <br>
-    <div class="row text-center bg-light">
-        <h3>Restaurant</h3>
-    </div>
-    <div class="row">
-        <div class="col-sm-6">
-            <img src="images/v.svg" class="img-fluid" alt="">
-        </div>
-        <div class="col-sm-6 bg-info" style="opacity:70%;">
-            <?php
-                if (file_exists('xml/cinema.xml')) {
-                    $xml = simplexml_load_file('xml/cinema.xml');
-                    echo $xml->film[0]->synopsis;
-                } else {
-                    exit('Echec lors de l\'ouverture du fichier test.xml.');
-                }
-            ?>
-        </div>
-    
-    </div>
-</div>
+    <div class="container">
+        <div class="accordion" id="accordionExample" style="margin: none;">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    Programme cinema
+                </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <?php
+                        if (file_exists('./xml/programme.xml')) {
+                            $xml = simplexml_load_file('./xml/programme.xml');
+                            // $films = new SimpleXMLElement($xml);
 
-<br>
+
+                            $film = $xml->film[0];
+
+                            echo '<strong>Nom: </strong>',$film['nom'],"<br>" ;
+                            echo '<strong>Genre:</strong>',$film['genre'],"<br>" ;
+                            echo '<strong>Duree: </strong>',$film['duree'],"<br>" ;
+                            echo '<strong>Realisateur: </strong>',$film['realisateur'],"<br>" ;
+                            echo '<strong>Langue: </strong>',$film['lang'],"<br>","<br>" ;
+
+                            echo "<strong>Les acteur du film: </strong>";
+                            foreach($xml->film->acteurs->acteur as $acteur ){
+                                echo '<br>',$acteur;
+                            }
+
+                            echo "<br><br>";
+
+                            echo "<strong>Annee: </strong>",$film->annee,"<br>";
+                            echo "<strong>Note presse: </strong>",$film->note['presse'],"<br>";
+                            echo "<strong>Note spectateur: </strong>",$film->note['spectateurs'],"<br><br>";
+
+                            echo "<strong>Synopsis: </strong>",$film->synopsis,"<br><br>";
+
+                            foreach($xml->film->projections->projection as $projection ){
+                                echo "<strong>Jour de projection: </strong>",$projection['jour'],"<br>";
+                                foreach($projection->horaire as $horaire ){
+                                    echo "<strong>Horaire: </strong>",$horaire,"<br>";
+                                }
+                                echo "<br>";
+                            }
+
+
+                        } else {
+                            exit('Echec lors de l\'ouverture du fichier testA.xml.');
+                        }
+                    ?>
+                </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Description restaurant
+                </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+
+                </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Examen
+                </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
     
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
--- process.php
+++
@@ -1,6 +0,0 @@
-<?php
-function php_func($x){
-    $x+=1;
-}
-php_func($x);
-?>
